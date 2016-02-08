24a419faf42874b20028a39477178bb19a3e407b
Rory Stokes <contact@rorystok.es>
2016-02-08 14:29:21 +1100

    Initial commit

---
+++ explorer.py
@@ -0,0 +1,58 @@
+import numpy
+
+#dictionary of cardinal directions indexed by their label character
+_directions = {"N":numpy.array((0,1)),"S":numpy.array((0,-1)),"E":numpy.array((1,0)),"W":numpy.array((-1,0))}
+#2x2 rotation matrices for the 90 degree rotations
+_left = numpy.array([[0,-1],[1,0]])
+_right = numpy.array([[0,1],[-1,0]])
+
+class Explorer:
+
+	def __init__(self,x,y,facing,bounds):
+		#position is stored in a numpy array for efficient modification
+		self.pos    = numpy.array((x,y))
+		#grid bounds are stored to ensure explorer does not leave grid
+		self.bounds = numpy.array(bounds)
+
+		if facing in _directions.keys():
+			#sets the direction to a unitary vector in the target direction, fetched from the cardinal direction directory
+			self.direction = _directions[facing]
+
+	def turnLeft(self):
+		#multiply direction vector by rotation matrix
+		self.direction = _left.dot(self.direction)
+
+	def turnRight(self):
+		#multiply direction vector by rotation matrix
+		self.direction = _right.dot(self.direction)
+
+	def move(self):
+		#add the current direction vector to position
+		target = self.pos + self.direction
+
+		#test to see if new position is within bounds
+		if (target>self.bounds[2:3]).any() or (target<self.bounds[0:1]).any():
+			#if not, output an error and make no movement
+			print("Error: movement out of bounds")
+			return False
+		else:
+			#if position within bounds, make the movement
+			self.pos = target
+			return True
+
+	def getX(self):
+		#returns the x component of the current position
+		return self.pos[0]
+
+	def getY(self):
+		#returns the y component of the current position
+		return self.pos[1]
+
+	def getFacing(self):
+		#fetches the relevant character from the cardinal direction dictionary and returns
+		for char, vector in _directions.items():
+			if numpy.array_equal(vector, self.direction):
+				return char
+		#if no matches are found (should not be possible), display an error
+		print("Error: facing in non-cardinal direction")
+		return 'X'
---
+++ main.py
@@ -0,0 +1,94 @@
+from explorer import Explorer
+import sys, re
+
+
+#defining regular expressions to read in each of the 3 different input line types
+gridInitPattern     = re.compile("([0-9]+) ([0-9]+)$")
+explorerInitPattern = re.compile("([0-9]+) ([0-9]+) ([NSEW])$")
+explorerMovePattern = re.compile("([LRM])*")
+
+
+######################
+#FIRST LINE OF INPUT #
+######################
+
+#uses the regular expression to read the first line of input
+match = gridInitPattern.match(input())
+#if the regex does not match, output an error and quit
+if match is None:
+	print("Invalid input in line 1. Line should contain: [right coord] [top coord]")
+	sys.exit(1)
+#extract the grid size from the line. these values are not currently used
+right  = int(match.group(1))
+top    = int(match.group(2))
+#store grid bounds as a tuple
+bounds = (0,0,right,top)
+
+#############################
+# SUBSEQUENT LINES OF INPUT #
+#############################
+
+#initialise variables
+lineNumber = 1
+explorers  = []
+
+for line in sys.stdin:
+
+	## LINE 1: INITIALISATION ##
+
+	#Input is converted to upper case to allow for mixed case entry,
+	#since behaviour is still well defined
+	init = explorerInitPattern.match(line.upper())
+	lineNumber += 1
+
+	#if the regex does not match, output an error and quit
+	if init is None:
+		print("Invalid input in line {}. Line should contain: [x coord] [y coord] [facing direction (N,S,E,W)]".format(lineNumber))
+		sys.exit(1)
+	#extract the values from the line
+	x = int(init.group(1))
+	y = int(init.group(2))
+	facing = init.group(3)
+	#construct the explorer object from the extracted values
+	currentExplorer = Explorer(x,y,facing,bounds)
+	#store the explorer in a list of explorers
+	explorers.append(currentExplorer)
+
+
+	## LINE 2: MOVEMENT INSTRUCTIONS ##
+
+	#Input is converted to upper case to allow for mixed case entry,
+	#since behaviour is still well defined
+	line = input().upper()
+	lineNumber += 1
+
+	#use regex to confirm that string only contains valid instructions
+	move = explorerMovePattern.match(line)
+	#if the regex does not match, output an error and quit
+	if move is None:
+		print("Invalid input in line {}. Line should only contain characters 'L', 'R' and 'M'".format(lineNumber))
+		sys.exit(1)
+
+	#iterate through each character and call relevant method on explorer object
+	for char in line:
+		if char == "L":
+			currentExplorer.turnLeft()
+		elif char == "R":
+			currentExplorer.turnRight()
+		else:
+			#Since the line matched the move pattern, the only other option is moving
+			if currentExplorer.move() is False:
+				print("Error: Invalid movement in line {}, terminating".format(lineNumber))
+				sys.exit(1)
+
+
+##########
+# OUTPUT #
+##########
+
+#defining output format string: two numbers and a single character
+outputFormat = "%d %d %s"
+
+#iterate through the explorers in the list
+for explorer in explorers:
+	print(outputFormat % (explorer.getX(), explorer.getY(), explorer.getFacing()))
